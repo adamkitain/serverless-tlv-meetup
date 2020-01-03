d8032283f48fe648bea7b3bdc6d1c22758f79c8c
Adam Kitain <adam.kitain@ridewithvia.com>
2020-01-03 11:56:49 +0200

    fixed alb response formatting, added epsagon

--- functions/hello_world.py
+++ functions/hello_world.py
@@ -1,7 +1,19 @@
 import logging
+import json
+
 logger = logging.getLogger()
+
 
 def handle(event, context):
     logger.info(event)
     logger.info(context)
-    return "Hello, world"
+    response = {
+        "statusCode": 200,
+        "body": json.dumps({"data": 3}),
+        "headers": {
+            "Content-Type": "application/json"
+        }
+    }
+
+    return response
+
